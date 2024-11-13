333425a3adff30cea9f2fb1e7e5361802b75b470
scalli007YT <pascal.rose007@web.de>
2024-11-13 15:28:52 +0100

    Before Applying Sites

--- app.vue
+++ app.vue
@@ -1,22 +1,9 @@
 <template>
-  <UContainer>
-    <UCard class="mt-10">
-      <template #header>
-        <div class="flex justify-between">
-          <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme>
-            <USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" />
-          </ColorScheme>
-        </div>
-      </template>
-      <!-- Use @click instead of onclick -->
-      <UButton icon="i-heroicons-book-open" @click="handleLogin" target="_blank">Click</UButton>
-    </UCard>
-  </UContainer>
 </template>
 
 <script setup lang="ts">
-const { signIn, signOut } = useAuth()
+const { signIn, signOut, data } = useAuth()
+
 
 function handleLogin() {
   signIn('github')
