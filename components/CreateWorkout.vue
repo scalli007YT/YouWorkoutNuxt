832f4f758b76cf9c30f3f73f0fef2b3dc1d5123c
scalli007YT <pascal.rose007@web.de>
2024-11-14 23:18:18 +0100

    Styling for Dialog

--- components/CreateWorkout.vue
+++ components/CreateWorkout.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-card flat append-icon="mdi-plus" class="border-dashed rounded-xl" style="border-width: 3px;"
-    subtitle="No Workout found in library" target="_blank" title="Create New Workout" @click="dialog = true">
+  <v-card flat append-icon="mdi-plus" class="border-dashed rounded-xl pa-1" style="border-width: 3px; font-size: 1.5em;"
+    subtitle="No Workout found in library" title="Create New Workout" @click="dialog = true">
   </v-card>
 
   <v-dialog v-model="dialog" max-width="600">
     <v-card prepend-icon="mdi-dumbbell" append-icon="mdi-dumbbell" title="Create Workout"
-      class="mx-auto pa-3 max-w-2xl rounded-xl text-center">
+      class="mx-auto pa-3 max-w-2xl rounded-xl text-center text-base">
+
       <v-divider style="margin-bottom: 24px;"></v-divider>
 
       <v-card-text>
-
         <v-row dense>
           <v-col cols="12" md="4" sm="6">
             <v-text-field label="First name*" required></v-text-field>
@@ -42,9 +42,7 @@
       <v-divider></v-divider>
 
       <v-card-actions>
-
         <v-btn text="Close" variant="plain" @click="dialog = false"></v-btn>
-
         <v-btn color="primary" text="Save" variant="tonal" @click="dialog = false"></v-btn>
       </v-card-actions>
     </v-card>
