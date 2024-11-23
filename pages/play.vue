7143ca9dd9eeef9aebee9b760e1326233c943829
scalli007YT <pascal.rose007@web.de>
2024-11-23 18:26:56 +0100

    Stepper Template

--- pages/play.vue
+++ pages/play.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
+  <v-container>
+    <v-card elevation="8" class="mx-auto pa-6 max-w-full rounded-xl border">
+      <v-stepper flat :items="['Select', 'Settings', 'Start']">
 
-  </div>
+        <template v-slot:item.1>
+          <WorkoutList />
+        </template>
+
+        <template v-slot:item.2>
+          <v-card title="Step Two" flat>...</v-card>
+        </template>
+
+        <template v-slot:item.3>
+          <v-card title="Step Three" flat>...</v-card>
+        </template>
+      </v-stepper>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
